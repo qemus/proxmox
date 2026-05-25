31c1d737c610dc62dde4f32ae28aaa1700e66d83
Kroese <kroese@users.noreply.github.com>
2026-05-25 22:18:10 +0200

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -66,8 +66,13 @@ fi
 # Update username and password
 printf '%s:%s\n' "$USERNAME" "$PASSWORD" | chpasswd
 
-for i in $(ip -o link show | awk -F': ' '{print $2}' | grep -v lo | sed 's/@.*//'); do 
-  echo -e "auto $i\niface $i inet manual\n" # >> /etc/network/interfaces
+for i in $(ip -o link show | awk -F': ' '{print $2}' | grep -v lo | sed 's/@.*//'); do
+  if ! grep -qE "^iface[[:space:]]+$i[[:space:]]" /etc/network/interfaces; then
+    echo -e "auto $i\niface $i inet manual\n" >> /etc/network/interfaces
+  fi
 done
+
+cat /etc/network/interfaces
+sleep 10
 
 exec /usr/sbin/init 3
