400ed41a571ea7a6dcdc72854064728d89672dd7
Kroese <kroese@users.noreply.github.com>
2026-05-26 15:57:37 +0200

    feat: Improved networking auto-configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,10 @@ warn () { printf "%b%s%b" "\E[1;31m❯ " "Warning: ${1:-}" "\E[0m\n" >&2; }
 # Display version number
 info "❯ Starting Proxmox for Docker v$(</run/version)..."
 info "❯ For support visit https://github.com/dockur/proxmox"
-info ""
+echo ""
+
+# Update password for root
+printf 'root:%s\n' "$PASSWORD" | chpasswd
 
 # Get the capability bounding set
 CAP_BND=$(grep '^CapBnd:' /proc/$$/status | awk '{print $2}')
@@ -29,6 +32,7 @@ LAST_CAP=$(cat /proc/sys/kernel/cap_last_cap)
 # Calculate the maximum capability value
 MAX_CAP=$(((1 << (LAST_CAP + 1)) - 1))
 
+# Check if container is privileged
 if [ "${CAP_BND}" -ne "${MAX_CAP}" ]; then
   error "Please start the container with the --privileged flag!"
   [[ "${DEBUG:-}" != [Yy1]* ]] && exit 14
@@ -61,9 +65,6 @@ if [ -n "$KVM_ERR" ]; then
   [[ "${DEBUG:-}" != [Yy1]* ]] && exit 19
 fi
 
-# Update password for root
-printf 'root:%s\n' "$PASSWORD" | chpasswd
-
 # Modify setting for LXC containers
 file="/lib/systemd/system/lxcfs.service"
 
@@ -73,55 +74,102 @@ if grep -qE '^[[:space:]]*ConditionVirtualization' "$file"; then
     sed -i '/^[[:space:]]*ConditionVirtualization/ {
         /^[[:space:]]*#/! s/^[[:space:]]*/#/
     }' "$file"
-
 fi
 
-# Automaticly add network interfaces
-file="/tmp/interfaces.tmp"
+# Check if Docker socket is available
+if [ ! -S /var/run/docker.sock ]; then
 
-echo "auto lo" > "$file"
-echo "iface lo inet loopback" >> "$file"
-echo "" >> "$file"
+  error "Docker socket is missing? Please bind /var/run/docker.sock in your compose file." 
+  warn "will skip networking configuration, as it requires the Docker socket to be available."
 
-ip -o link show | awk -F': ' '{print $2}' | grep -v lo | sed 's/@.*//' | while IFS= read -r i; do
+else
+
+  # Create a bridge network called proxnet if not exist
   
-  printf 'auto %s\niface %s inet manual\n\n' "$i" "$i" >> "$file"
+  net="proxnet"
+  docker network rm "$net" &>/dev/null || true
+  
+  if ! docker network inspect "$net" &>/dev/null; then
+  
+    if ! docker network create --driver=bridge "$net" >/dev/null; then
+      error "Failed to create bridge network '$net'!" && exit 29
+    fi
+  
+    if ! docker network inspect "$net" &>/dev/null; then
+      error "Bridge network '$net' does not exist?" && exit 30
+    fi
+  fi
+  
+  # Determine container name
+  target=$(hostname -s)
+  target=$(docker inspect -f '{{.Name}} {{.Config.Hostname}}' $(docker ps -aq) | awk '$2=="'"$target"'"{print $1}' | tail -c+2)
+  
+  if ! docker inspect "$target" &>/dev/null; then
+    error "Failed to find a container with name: '$target'!" && exit 31
+  fi
+  
+  resp=$(docker inspect "$target")
+  network=$(echo "$resp" | jq -r ".[0].NetworkSettings.Networks[\"$net\"]")
+  
+  if [ -z "$network" ] || [[ "$network" == "null" ]]; then
+    if ! docker network connect "$net" "$target"; then
+      error "Failed to connect container to bridge network '$net'!" && exit 32
+    fi
+  fi
+  
+  # Determine subnet and gateway
+  inspect=$(docker network inspect "$net")
+  subnet=$(echo "$inspect" | jq -r '.[0].IPAM.Config[0].Subnet')
+  gateway=$(echo "$inspect" | jq -r '.[0].IPAM.Config[0].Gateway')
+  
+  # Automaticly add all network interfaces
+  file="/etc/network/interfaces.new"
+  
+  echo "auto lo" > "$file"
+  echo "iface lo inet loopback" >> "$file"
+  
+  while IFS= read -r i; do
+  
+    echo "" >> "$file"
+    echo "auto $i" >> "$file"
+    echo "iface $i inet manual" >> "$file"
+  
+  done <<< $(ip -o link show | awk -F': ' '{print $2}' | grep -v lo | sed 's/@.*//')
+  
+  echo "" >> "$file"
+  
+  # Determine which interface is our bridge network
+  bridge=""
+  base="${subnet%.*}."
+  
+  while read -r _ iface _ addr _; do
+  
+    # Check if IP belongs to our subnet
+    if [[ "${addr%.*}.${subnet/$base/}" == "$subnet" ]]; then
+  
+      if [ -z "$bridge" ]; then
+        bridge="$iface"
+      else
+        error "Found multiple interfaces to our bridge network?"
+      fi
+  
+    fi
+  
+  done <<< $(ip -o -4 addr show)
+  
+  [ -z "$bridge" ] && error "Could not find interface of bridge?" && exit 35
+  
+  # Configure bridge
+  echo "auto docker0" >> "$file"
+  echo "iface docker0 inet static" >> "$file"
+  echo "        address $subnet" >> "$file"
+  echo "        gateway $gateway" >> "$file"
+  echo "        bridge-ports $bridge" >> "$file"
+  echo "        bridge-stp off" >> "$file"
+  echo "        bridge-fd 0" >> "$file"
+  echo "" >> "$file"
 
-done
-
-NET_DEV=""
-
-# Give Kubernetes priority over the default interface
-[ -d "/sys/class/net/net0" ] && NET_DEV="net0"
-[ -d "/sys/class/net/net1" ] && NET_DEV="net1"
-[ -d "/sys/class/net/net2" ] && NET_DEV="net2"
-[ -d "/sys/class/net/net3" ] && NET_DEV="net3"
-
-# Automatically detect the default network interface
-[ -z "$NET_DEV" ] && NET_DEV=$(awk '$2 == 00000000 { print $1 }' /proc/net/route)
-[ -z "$NET_DEV" ] && NET_DEV="eth0"
-
-if [ ! -d "/sys/class/net/$NET_DEV" ]; then
-  error "Network interface '$NET_DEV' does not exist inside the container!" && exit 26
 fi
 
-# Detect IP address
-{ IP=$(ip address show dev "$NET_DEV" | grep inet | awk '/inet / { print $2 }' | cut -f1 -d/ | head -n 1); rc=$?; } 2>/dev/null || :
-
-if (( rc != 0 )); then
-  error "Could not determine container IP address!" && exit 27
-fi
-
-# Create bridge 
-# echo "auto docker0" >> "$file"
-# echo "iface docker0 inet static" >> "$file"
-# echo "        address ${IP%.*}.0/24" >> "$file"
-# echo "        gateway ${IP%.*}.1" >> "$file"
-# echo "        bridge-ports $NET_DEV" >> "$file"
-# echo "        bridge-stp off" >> "$file"
-# echo "        bridge-fd 0" >> "$file"
-
-# Apply configuration
-cp "$file" /etc/network/interfaces.new
-
+# Boot systemd
 exec /sbin/init 3
